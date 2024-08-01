493fc476d373db1f4dea8dc8ab68a9eaa24abab8
Siew Kam Onn <kosiew@gmail.com>
2024-08-01 11:35:44 +0800

    Add FilterQuotesPipelineLoveOrLife pipeline

--- quote_crawl/quote_crawl/pipelines.py
+++ quote_crawl/quote_crawl/pipelines.py
@@ -6,8 +6,20 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
 class QuoteCrawlPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class FilterQuotesPipelineLoveOrLife:
+    def process_item(self, item, spider):
+        # Check if 'love' or 'life' is in the text
+        if "love" in item["title"].lower() or "life" in item["title"].lower():
+            return item
+        else:
+            raise DropItem(
+                f"Quote without 'love' or 'life' in title text: {item['title']}"
+            )

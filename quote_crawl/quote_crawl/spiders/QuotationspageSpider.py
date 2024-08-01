d44354efb5099229b4bd9c2a540c09c974a63e4b
Siew Kam Onn <kosiew@gmail.com>
2024-08-01 14:20:23 +0800

    test crawl

--- quote_crawl/quote_crawl/spiders/QuotationspageSpider.py
+++ quote_crawl/quote_crawl/spiders/QuotationspageSpider.py
@@ -20,7 +20,7 @@ class QuotationspageSpider(scrapy.Spider):
     def parse(self, response):
         for main in response.css('dl'):
             quotes = main.css('dt.quote a::text').getall()
-            authors = main.css('dd.author a::text').getall()
+            authors = main.css('dd.author b a::text').getall()
             for quote, author in zip(quotes, authors):
                 loader = QuoteLoader(item=QuoteCrawlItem())
                 loader.add_value("title", quote)

431091fd5e3e1b1ed3851fc6a5c1e7a73128d169
Siew Kam Onn <kosiew@gmail.com>
2024-08-01 13:41:20 +0800

    Azquotes - Add user-agent

--- quote_crawl/quote_crawl/spiders/AzquotesSpider.py
+++ quote_crawl/quote_crawl/spiders/AzquotesSpider.py
@@ -10,6 +10,12 @@ class AzquotesSpider(scrapy.Spider):
     start_urls = [
         "https://www.azquotes.com/quotes/topics/life.html",
     ]
+    def start_requests(self):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        }
+        for url in self.start_urls:
+            yield scrapy.Request(url, headers=headers)
 
     def parse(self, response):
         for quote in response.css('div.wrap-block'):

73fc44e39b244054481193c81b5b940eeb71ff7d
Siew Kam Onn <kosiew@gmail.com>
2024-08-01 13:40:12 +0800

    ignore robots

--- quote_crawl/quote_crawl/settings.py
+++ quote_crawl/quote_crawl/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "quote_crawl.spiders"
 # USER_AGENT = "quote_crawl (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32

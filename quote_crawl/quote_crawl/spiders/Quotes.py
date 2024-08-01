e9f2876228394e72d180bae3f503c269dbe7939a
Siew Kam Onn <kosiew@gmail.com>
2024-08-01 10:58:23 +0800

    move Quotes to spiders

---
+++ quote_crawl/quote_crawl/spiders/Quotes.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "Quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/"]
+
+    def parse(self, response):
+        # each quote is within <div class="quote" ...>
+        quotes = response.css("div.quote")
+        for quote in quotes:
+            # each quote text is within <span class="text" ...>
+            title = quote.css("span.text::text").get()
+            # each author info is within <small class="author" ...>
+            author = quote.css("small.author::text").get()
+            yield {"title": title, "author": author}

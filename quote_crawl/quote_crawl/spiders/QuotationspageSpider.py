8a4f9640ae43c5f34a1c3a7521f244fb55f4e318
Siew Kam Onn <kosiew@gmail.com>
2024-08-01 13:50:21 +0800

    QuotationsPage fetched all quotations

--- quote_crawl/quote_crawl/spiders/QuotationspageSpider.py
+++ quote_crawl/quote_crawl/spiders/QuotationspageSpider.py
@@ -1,8 +1,7 @@
-
-
 import scrapy
 from quote_crawl.items import QuoteCrawlItem
 from quote_crawl.items_loader import QuoteLoader
+
 
 class QuotationspageSpider(scrapy.Spider):
     name = "QuotationsPage"
@@ -10,6 +9,7 @@ class QuotationspageSpider(scrapy.Spider):
     start_urls = [
         "http://www.quotationspage.com/qotd.html",
     ]
+
     def start_requests(self):
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -18,8 +18,11 @@ class QuotationspageSpider(scrapy.Spider):
             yield scrapy.Request(url, headers=headers)
 
     def parse(self, response):
-        for quote in response.css('dl'):
-            loader = QuoteLoader(item=QuoteCrawlItem(), selector=quote)
-            loader.add_css("title", "dt.quote a::text")
-            loader.add_css("author", "dd.author a::text")
-            yield loader.load_item()
+        for dl in response.css("dl"):
+            quotes = dl.css("dt.quote a::text").getall()
+            authors = dl.css("dd.author a::text").getall()
+            for quote, author in zip(quotes, authors):
+                loader = QuoteLoader(item=QuoteCrawlItem())
+                loader.add_value("title", quote)
+                loader.add_value("author", author)
+                yield loader.load_item()
